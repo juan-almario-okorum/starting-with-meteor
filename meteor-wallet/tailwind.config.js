81ec396f9b7751f86c854a2fa79b001eed4a15a8
juan-almario-okorum <juan.almario@okorum.com>
2025-06-25 13:28:03 -0500

    cambios

--- meteor-wallet/tailwind.config.js
+++ meteor-wallet/tailwind.config.js
@@ -1,7 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./client/**/*.{js,jsx}', './imports/**/*.{js,jsx}'],
+  content: [
+    "./imports/ui/**/*.{js,jsx,ts,tsx}",
+    "./client/**/*.{js,jsx,ts,tsx}",
+    "./server/**/*.{js,jsx,ts,tsx}",
+    "./**/*.html"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
