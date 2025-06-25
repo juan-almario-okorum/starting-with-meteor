e5416ef17c5b76a2b83c65bb4bf6b3050a1948e7
juan-almario-okorum <juan.almario@okorum.com>
2025-06-24 21:23:01 -0500

    se intentó agregar tailwind

--- meteor-wallet/imports/ui/App.jsx
+++ meteor-wallet/imports/ui/App.jsx
@@ -5,6 +5,10 @@ import { ContactList } from './contactList';
 export const App = () => (
   <div>
     <h1>Meteor Wallet</h1>
+    <h1 className="bg-red-500 text-white text-4xl p-4 rounded-xl">
+  ¡Tailwind finalmente funcionando!
+</h1>
+
     <ContactForm/>
     <ContactList/>
   </div>
---
+++ meteor-wallet/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./client/**/*.{js,jsx}', './imports/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
